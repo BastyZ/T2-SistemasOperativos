2ebd461f1e4489e4d8c952720f94417dda3e497e
BastyZ <bastian.inostroza@gmail.com>
2018-05-15 18:03:45 -0400

    cambio push por put en el primer caso ahora si

--- nExchange.c
+++ nExchange.c
@@ -67,7 +67,7 @@ void* nExchange(nTask task, void *msg, int timeout) {
                 ProgramTask(timeout);
             } else this_task->status = WAIT_EXCHANGE;
             //nPrintf("Primero: voy a hacer push");
-            PushObj(task->exchange_queue, this_task);
+            PutObj(task->exchange_queue, this_task);
             //nPrintf("Primero: chao loh vimoh\n");
             ResumeNextReadyTask();
         }
